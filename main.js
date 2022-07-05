9b8f8586de00ea5344b45d24481f430a88ec1aff
Nazgul Zhuzumova <93662157+NazgulZhuzumova@users.noreply.github.com>
2022-07-05 19:02:02 +0100

    Add files via upload
    javascript practice

---
+++ main.js
@@ -0,0 +1,31 @@
+/*const dayOfWeek = prompt('Enter a day').toUpperCase;
+
+if (dayOfWeek === 'Monday') {
+    console.log("Oh noo it's Monday");
+else if (dayOfWeek === "Saturday"){
+    console.log("Yey I love saturdays")
+}
+else if (dayOfWeek === "Friday") {
+    console.log("Fridays are great, especially after work!")
+} else {
+    console.log('Meh')
+}*/
+/*const age = 0;
+if (age < 5){
+    console.group("You are a child free entrance!")
+} else if(age < 10) {
+    console.log("You are not fee free! please pay fee.")
+} else if(age < 65){
+    console.log("you are an adult. You pay $20")
+}else {
+    console.log('You are a senior. You pay $10')
+}*/
+
+const color ='Enter a color';
+if (color === "pink"){
+    console.log("Beautiful color!")
+}else if(color === "green"){
+    console.log("Refreshing color!")
+} else{
+    console.log("RAINBOW!")
+}
